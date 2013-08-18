bf3e68e83dc7701aafe369d4e232c2bd4837766c
wooque <wooque@gmail.com>
2013-08-18 20:53:55 +0200

    Little change in Assembler test

--- src/sim/assembler/Test.java
+++ src/sim/assembler/Test.java
@@ -3,7 +3,7 @@ package sim.assembler;
 public class Test {
 
 	public static void main(String[] args) {
-		Assembler asm = new Assembler("asm/test2.asm", null);
+		Assembler asm = new Assembler("asm/test.asm", null);
 		new Disassembler(asm.getCode(), asm.getStartOfCode());
 	}
 }
